95f9aafeb7f930228d0a8e37d4f7ce4763358080
Shaun McKinnon <shaun.mckinnon@georgiancollege.ca>
2024-03-04 13:48:30 -0500

    fix: minor issues

--- website/app.js
+++ website/app.js
@@ -1,18 +1,19 @@
-import express from "express";
-import path from "path";
-import { fileURLToPath } from "url";
+import express from "express"; // Importing Express framework
+import path from "path"; // Path module for handling file paths
+import { fileURLToPath } from "url"; // Utility function to convert file URLs to paths
 
+// Getting the current filename and directory path
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const app = express();
+const app = express(); // Creating an Express application
 
-// Serve static files from the React app
+// Serving static files from the React app build directory
 app.use(express.static(path.join(__dirname, 'build')));
 
-// Handles any requests that don't match the ones above
+// Handling any requests by serving the React app's main HTML file
 app.get('*', (_, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
-export default app;
+export default app; // Exporting the Express app for use in other files
