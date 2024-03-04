eec9349ce186b298905e13d2fd6596749c4a6b7b
Shaun McKinnon <shaun.mckinnon@georgiancollege.ca>
2024-03-04 15:04:19 -0500

    feat: setup framework for comp-2068 react app

--- website/app.js
+++ website/app.js
@@ -9,11 +9,9 @@ const __dirname = path.dirname(__filename);
 const app = express(); // Creating an Express application
 
 // Serving static files from the React app build directory
-app.use(express.static(path.join(__dirname, 'build')));
+
 
 // Handling any requests by serving the React app's main HTML file
-app.get('*', (_, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
+
 
 export default app; // Exporting the Express app for use in other files
