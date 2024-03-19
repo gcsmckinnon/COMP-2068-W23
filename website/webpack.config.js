2f3ffe4427119957d9f63e78e9fb87230cc43904
Shaun McKinnon <shaun.mckinnon@georgiancollege.ca>
2024-03-19 04:31:34 -0400

    fix: issue with refresh

--- website/webpack.config.js
+++ website/webpack.config.js
@@ -22,6 +22,7 @@ const config = {
     output: {
         path: path.resolve(__dirname, "build"),
         filename: "bundle.js",
+        publicPath: "/",
     },
     // Specify the file extensions that can be imported without specifying the extension
     resolve: {
